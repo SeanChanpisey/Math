66ef8c5499f5c32c9b16cad7cac3102807959fd8
Admin <Admin@SeanChanpisey>
2024-02-22 08:57:53 +0700

    final

--- finalJava/src/finalJava/ExcerciseTwo.java
+++ finalJava/src/finalJava/ExcerciseTwo.java
@@ -48,13 +48,6 @@ public class ExcerciseTwo {
 	        System.out.println("Total Expense: $" + getTotalExpense());
 
 	    }
-	       public static void main(String[] args) {
-	        Customer customer = new Customer("Drake", "Premium");
-	        Sale sale = new Sale(customer, "29-01-2024");
-	        sale.setServiceExpense(100.0);
-	        sale.setProductExpense(550.0);
-
-	        sale.DisplayInfo();
-	    }
+	       
 	}
 }
